251f86261a77c7159628b0d28922d3bb3ce87cbd
adminlocal <adminlocal@server.esdu.ru>
2024-08-16 03:55:33 +0300

    новый файл:    reactapp/.gitignore 	новый файл:    reactapp/README2.md 	новый файл:    reactapp/package-lock.json 	новый файл:    reactapp/package.json 	новый файл:    reactapp/public/favicon.ico 	новый файл:    reactapp/public/index.html 	новый файл:    reactapp/public/logo192.png 	новый файл:    reactapp/public/logo512.png 	новый файл:    reactapp/public/manifest.json 	новый файл:    reactapp/public/robots.txt 	новый файл:    reactapp/src/app/App.css 	новый файл:    reactapp/src/app/App.js 	новый файл:    reactapp/src/app/App.test.js 	новый файл:    reactapp/src/appHeader/Header.js 	новый файл:    reactapp/src/appHome/Home.js 	новый файл:    reactapp/src/appListProblems/ListProblems.js 	новый файл:    reactapp/src/appModalProblem/ModalProblem.js 	новый файл:    reactapp/src/appPhotoModal/ModalPhoto.js 	новый файл:    reactapp/src/appProblemForm/ProblemForm.js 	новый файл:    reactapp/src/appRemoveProblem/appRemoveProblem.js 	новый файл:    reactapp/src/index.css 	новый файл:    reactapp/src/index.js 	новый файл:    reactapp/src/logo.svg 	новый файл:    reactapp/src/reportWebVitals.js 	новый файл:    reactapp/src/setupTests.js

---
+++ reactapp/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './app/App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export const API_URL = "http://127.0.0.1:8000/"
+export const API_STATIC_MEDIA = "http://127.0.0.1:8000/problems"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
+
+reportWebVitals();
